87226f0af033fd341b58aea0fcbade688e0b0ad8
Aditya Singh <adi.sh5442@gmail.com>
2022-07-03 18:08:23 -0700

    unique occuerence of an element in an array

---
+++ bit_manipulation/Unique_occurence_in_array.py
@@ -0,0 +1,13 @@
+'''The aim of this program is to find out thee number which occurs
+only 1 time in an array in which all other elements occurs twice
+USING BITWISE'''
+
+array = list(map(int,input("Enter the elements of the array:").split()))
+if len(array) ==0:
+    print("PLease enter a non-zero length array")
+else:
+    num=array[0];
+    for i in range(1,len(array)):
+        num^=array[i]
+print("The number occuring only 1 time is:",num)
+
